4864bc7add14becc50bb1eabdf22f1bb7279c6e4
Pk-Naveen <naveen.pk@prodapt.com>
2023-09-28 14:28:48 +0530

    login page with landing page connection

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'landingPage', pathMatch:'full'},
+  {component:LoginPageComponent,path:"Login"},
+  {path:'landingPage', component:LandingPageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
