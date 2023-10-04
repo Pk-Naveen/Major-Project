a3b086ef682cbb3fa1b09a5e31c42bf2fb2c294b
Guruprakashsgp <guruprakash.s@prodapt.com>
2023-10-04 16:55:45 +0530

    signup done need some changes

--- src/app/user-service.service.ts
+++ src/app/user-service.service.ts
@@ -10,5 +10,13 @@ export class UserServiceService {
   getUserData(username:any,password:any)
   {
     return this.http.get('http://localhost:8000/user/'+username+'/'+password);  // change the port according to ur spring boot running por
- 
+  }
+
+  userRegistration(Name:any,UserName:any,Password:any,EmailAddress:any)
+
+  {
+
+    return this.http.post(`http://localhost:8000/user/${Name}/${UserName}/${Password}/${EmailAddress}`,{});
+
+  }
 }
