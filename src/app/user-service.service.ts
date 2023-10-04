97f5b3f3606575607d5e324f6e269dccd0718ac7
Pk-Naveen <naveen.pk@prodapt.com>
2023-10-04 16:19:55 +0530

    resovle conflicts

--- src/app/user-service.service.ts
+++ src/app/user-service.service.ts
@@ -9,7 +9,7 @@ export class UserServiceService {
   constructor(private http:HttpClient) { }
   getUserData(username:any,password:any)
   {
-    return this.http.get('http://localhost:8010/user/'+username+'/'+password);  // change the port according to ur spring boot running port
+    return this.http.get('http://localhost:8000/user/'+username+'/'+password);  // change the port according to ur spring boot running port
   }
 
   userRegistration(Name:any,UserName:any,Password:any,EmailAddress:any)
